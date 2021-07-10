997b9fa10d360c120a64ef210f414b658d03a415
Taras K <neshrow.dev@gmail.com>
2021-07-11 00:07:49 +1000

    Правки форматирования кода
    add yapf format

--- 1-1.7/1-1.7.py
+++ 1-1.7/1-1.7.py
@@ -1,9 +1,9 @@
 #Блок 1-1.7
-print ('Задание №1')
-print (int(2.99))
+print('Задание №1')
+print(int(2.99))
 #
-print ('Задание №2')
-print (int(+-1.6))
+print('Задание №2')
+print(int(+-1.6))
 #
-print ('Задание №3')
-print (9 ** 19 - int(float(9 ** 19)))
+print('Задание №3')
+print(9**19 - int(float(9**19)))
--- 1-1.8/1-1.8.py
+++ 1-1.8/1-1.8.py
@@ -8,9 +8,9 @@ X = int(input())
 print(X // 60)
 print(X % 60)
 #Задание №3
-X = int(input()) #желаемое
-H = int(input()) * 60 #часы переводим в минуты
-M = int(input()) #минуты
+X = int(input())  #желаемое
+H = int(input()) * 60  #часы переводим в минуты
+M = int(input())  #минуты
 total = X + H + M
 print(total // 60)
 print(total % 60)
