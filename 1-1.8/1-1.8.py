ebcb41c72c238222ab6473240aa93c46fdca88f1
Taras K <neshrow.dev@gmail.com>
2021-07-10 20:40:30 +1000

    Задания блока 1-1.8
    Выполнено 3/3

---
+++ 1-1.8/1-1.8.py
@@ -0,0 +1,16 @@
+#Блок 1-1.8
+#Задание №1
+X = int(input())
+Y = int(input())
+print(X * 60 + Y)
+#Задание №2
+X = int(input())
+print(X // 60)
+print(X % 60)
+#Задание №3
+X = int(input()) #желаемое
+H = int(input()) * 60 #часы переводим в минуты
+M = int(input()) #минуты
+total = X + H + M
+print(total // 60)
+print(total % 60)
